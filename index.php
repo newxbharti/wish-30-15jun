733e73ebcaac55868f717173056f657d98002add
newxbharti <marydavid86439@gmail.com>
2026-06-15 17:59:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5033285,4,511,95,18,00000000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5033285&101" alt="free web tracker" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 <title>Wish</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
